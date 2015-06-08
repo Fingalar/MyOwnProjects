ea53a8a4f15d2c4ec9b3d2e4490a7e841a234332
pi <pi@debian>
2015-06-08 18:23:03 +0200

    resolve

--- computer_v1/computer_v1.h
+++ computer_v1/computer_v1.h
@@ -3,11 +3,27 @@
 
 # define PLUS 1
 # define MINUS -1
+# include "libft/libft.h"
+# include <stdio.h>
 
 typedef struct	s_number
 {
-	float		number;
+	double		number;
 	int			power;
 }				t_number;
 
+
+t_list		*parse_arg(char *arg);
+int		add_number(char *arg, t_list *expr, int i, int symbol);
+int		add_power(t_number *num, int i, char *arg);
+t_list		*reduced_form(t_list *expr_left, t_list *expr_right);
+void		reduce_expr(t_list *expr);
+void		reduce_right_to_zero(t_list *expr_right, t_list *expr_left);
+void	display_reduced_form(t_list *expr);
+void	sort_expr(t_list *expr);
+void		swap_numbers(t_elem *elem, t_elem *elem2);
+void		display_degree(t_list *expr);
+void		display_two_solutions(t_number *a, t_number *b, double discrimiant);
+void		find_discriminant_then_resolve(t_list *expr);
+void		resolve_polynomial(t_list *expr);
 #endif
--- computer_v1/main.c
+++ computer_v1/main.c
@@ -1,10 +1,79 @@
-#include "../libft/libft.h"
+#include "computer_v1.h"
+#include <stdlib.h>
+#include <math.h>
 
 int			main(int argc, char **argv)
 {
+	t_list 		*expr;
+
 	if (argc != 2)
 		return (1);
-	parse_arg(argv[1]);
+	expr = parse_arg(argv[1]);
+	resolve_polynomial(expr);
+}
+
+void		resolve_polynomial(t_list *expr)
+{
+	t_elem 		*elem;
+	int		max;
+	t_number	*value;
+
+	elem = expr->first;
+	value = ((t_number *)elem->value);
+	max = value->power;
+	while (elem != NULL)
+	{
+		value = ((t_number *)elem->value);
+		if (value->power > max && value->number != 0)
+			max = value->power;
+		elem = elem->next;
+	}
+//	if (max == 1)
+//		resolve_equation();
+	if (max == 2)
+		find_discriminant_then_resolve(expr);
+}
+
+void		find_discriminant_then_resolve(t_list *expr)
+{
+	double		discriminant;
+	t_elem 		*elemA;
+	t_elem 		*elemB;
+	t_elem 		*elemC;
+
+	elemA = expr->first;
+	elemB = elemA->next;
+	elemC = elemB->next;
+	discriminant = ((t_number *)elemB->value)->number 
+		* ((t_number *)elemB->value)->number - 4.0
+		* (((t_number *)elemA->value)->number 
+		* ((t_number *)elemC->value)->number);
+	//if (discrimant == 0)
+	//	display_single_solution(expr);
+	if (discriminant > 0)
+		display_two_solutions((t_number *)elemA->value, (t_number *)elemB->value, discriminant);
+	//if (discriminant < 0)
+	//	no_solution();
+}
+
+void		display_two_solutions(t_number *a, t_number *b, double discrimiant)
+{
+	double		solution1;
+	double		solution2;
+
+	solution1 = ((b->number * -1.0) - sqrt(a->number)) / (a->number * 2.0);
+	solution2 = ((b->number * -1.0) + sqrt(a->number)) / (a->number * 2.0);
+	printf("Discriminant is strictly positive, the two solutions are:\n");
+	printf("%.3g\n%.3g\n", solution1, solution2);
+}
+
+int		is_whitespace(char c)
+{
+	if (c == '\t' || c == ' ' || c == '\n')
+	{
+		return (1);	
+	}
+	return (0);
 }
 
 t_list		*parse_arg(char *arg)
@@ -15,15 +84,18 @@ t_list		*parse_arg(char *arg)
 	int			i;
 	int			symbol;
 
+	i = 0;
+	symbol = PLUS;
 	right = 0;
-	expr_left = ft_list_init();
-	expr_right = ft_list_init();
+	expr_left = ft_list_init(expr_left);
+	expr_right = ft_list_init(expr_right);
 	while (arg[i] != '\0')
 	{
 		if (arg[i] == ' ')
 			i++;
 		if (arg[i] == '=')
 		{
+			symbol = PLUS;
 			right = 1;
 			i++;
 		}
@@ -40,32 +112,42 @@ t_list		*parse_arg(char *arg)
 		else
 		{
 			if (right == 0)
-				i = add_number(*arg, expr_right, i, symbol);
+				i = add_number(arg, expr_right, i, symbol);
 			else
-				i = add_number(*arg, expr_left, i, symbol);
+				i = add_number(arg, expr_left, i, symbol);
 		}
 	}
-	reduced_form(expr_right, expr_left);
-	return (expr_right);
+	expr_left = reduced_form(expr_right, expr_left);
+	return (expr_left);
 }
 
 
 int		add_number(char *arg, t_list *expr, int i, int symbol)
 {
-	t_number		num;
+	t_number		*num;
 	t_elem			*elem;
+	char			*str;
+	int			j;
 
+	j = i;
+	num = ft_memalloc(sizeof(t_number));
 	if (arg[i] != 'X')
-		num.number = atof(arg + i) * symbol;
+	{
+		while (isdigit(arg[i]) || arg[i] == '.')
+			i++;
+		str = ft_strsub(arg, j, i -j);
+		num->number = atof(str) * symbol;
+		free(str);
+	}
 	else
-		num.number = 1 * symbol;
-	num.power = 0;
+		num->number = 1 * symbol;
+	num->power = 0;
 	while (isdigit(arg[i]) || arg[i] == '.' || is_whitespace(arg[i]))
 		i++;
 	while (arg[i] != '\0' && arg[i] != '=' && arg[i] != '+' && arg[i] != '-')
 	{
 		if (arg[i] == '*' || arg[i] == 'X')
-			i = add_power(&num, i, arg);
+			i = add_power(num, i, arg);
 		else
 			i++;
 	}
@@ -91,19 +173,27 @@ int		add_power(t_number *num, int i, char *arg)
 	return (i);
 }
 
-void		reduced_form(t_list *expr_left, t_list *expr_right)
+t_list		*reduced_form(t_list *expr_left, t_list *expr_right)
 {
 	t_elem		*elem;
 
 	if (expr_right->size == 1
 			&& ((t_number *)expr_right->first->value)->number == 0)
+	{
+		display_reduced_form(expr_left);
 		return (expr_left);
+	}
 	if (expr_left->size == 1
 			&& ((t_number *)expr_left->first->value)->number == 0)
-		return (expr->right);
+	{
+		display_reduced_form(expr_right);
+		return (expr_right);
+	}
 	reduce_expr(expr_left);
 	reduce_expr(expr_right);
 	reduce_right_to_zero(expr_right, expr_left);
+	display_reduced_form(expr_left);
+	return (expr_left);
 }
 
 void		reduce_expr(t_list *expr)
@@ -124,7 +214,7 @@ void		reduce_expr(t_list *expr)
 			{
 				num->number += ((t_number *)tmp->value)->number;
 				tmp2 = tmp->next;
-				ft_list_delone(tmp);
+				ft_list_delone(expr, tmp);
 				tmp = tmp2;
 			}
 			else
@@ -141,10 +231,12 @@ void		reduce_right_to_zero(t_list *expr_right, t_list *expr_left)
 	t_elem		*elem_left;
 	t_number	*num_right;
 	t_number	*num_left;
+	int		reduced;
 
 	elem_right = expr_right->first;
 	while (elem_right)
 	{
+		reduced = 0;
 		num_right = (t_number *)elem_right->value;
 		elem_left = expr_left->first;
 		while (elem_left)
@@ -152,21 +244,108 @@ void		reduce_right_to_zero(t_list *expr_right, t_list *expr_left)
 			num_left = (t_number *)elem_left->value;
 			if (num_left->power == num_right->power)
 			{
-				num_left->number += num_right->number;
-				tmp = elem_right->left;
+				num_left->number -= num_right->number;
+				tmp = elem_right->next;
 				ft_list_delone(expr_right, elem_right);
 				break;
 				elem_right = tmp;
+				reduced = 1;
 			}
 			elem_left = elem_left->next;
 		}
 		if (elem_left == NULL)
 		{
-			num_right *= -1;
+			num_right->number *= 1;
+			elem_right->previous->next = elem_right->next;
+			elem_right->next->previous = elem_right->previous;
 			ft_list_add(expr_left, elem_right);
 		}
-		elem_right = elem_right->next;
+		if (!reduced) 
+			elem_right = elem_right->next;
 	}
 }
 
+
+void	display_reduced_form(t_list *expr)
+{
+	t_elem		*elem;
+	t_number	*value;
+
+	sort_expr(expr);
+	printf("Reduced form : ");
+	elem = expr->first;
+	while(elem != NULL)
+	{
+		value = ((t_number *)elem->value);
+		if (elem != expr->first)
+			printf(" + ");
+		printf("%.2g", value->number);
+		if (value->power >= 1)
+			printf("X");
+		if (value->power >= 2)
+			printf("^%d", value->power);
+		elem = elem->next;
+	}
+	printf(" = 0\n");
+	display_degree(expr);
+}
+
+
+void	sort_expr(t_list *expr)
+{
+	t_elem		*elem;
+	t_elem		*elem2;
+
+	elem = expr->first;
+	while (elem != NULL)
+	{
+		elem2 = expr->first->next;
+		while (elem2 != NULL)
+		{
+			if (((t_number *)elem2->value)->power > ((t_number *)elem->value)->power)
+			{
+				swap_numbers(elem, elem2);	
+			}
+			elem2 = elem2->next;
+		}
+		elem = elem->next;
+	}
+}
+
+void		swap_numbers(t_elem *elem, t_elem *elem2)
+{
+	int		pow;
+	double		nbr;
+	
+	pow = ((t_number *)elem2->value)->power;
+	nbr = ((t_number *)elem2->value)->number;
+	((t_number *)elem2->value)->power = ((t_number *)elem->value)->power;
+	((t_number *)elem2->value)->number = ((t_number *)elem->value)->number;
+	((t_number *)elem2->value)->power = pow;
+	((t_number *)elem2->value)->number = nbr;
+}
+
+void		display_degree(t_list *expr)
+{
+	t_elem 		*elem;
+	int		max;
+	t_number	*value;
+
+	elem = expr->first;
+	value = ((t_number *)elem->value);
+	max = value->power;
+	while (elem != NULL)
+	{
+		value = ((t_number *)elem->value);
+		if (value->power > max && value->number != 0)
+			max = value->power;
+		elem = elem->next;
+	}
+	printf("Polynomial degree: %d\n", max);			
+	if (max >= 3)
+	{
+		printf("Polynomial degree est greater than 2, i can't solve it\n");
+		exit(0);
+	}
+}
 
